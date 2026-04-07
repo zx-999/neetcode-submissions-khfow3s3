24a1a5bb24d636626805ec3a076c85b4e3c75bff
zx-999 <166580700+zx-999@users.noreply.github.com>
2026-04-07 14:50:03 -0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+      topKFrequent(nums, k) {
+        const frequencyMap = nums.reduce((acc, value) => {
+            if (acc[value]) acc[value] +=1
+            else acc[value] = 1
+            return acc;
+        }, {})
+        return Object.keys(frequencyMap).sort((a, b) => frequencyMap[b]-frequencyMap[a]).slice(0,k)
+    }
+
+}
