59fc8176c6bf51e8c0beaa2f80cbbb27877200ca
zx-999 <166580700+zx-999@users.noreply.github.com>
2026-04-08 11:42:16 -0700

    Update: top-k-elements-in-list - submission-11

--- Data Structures & Algorithms/top-k-elements-in-list/submission-11.js
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.js
@@ -4,13 +4,34 @@ class Solution {
      * @param {number} k
      * @return {number[]}
      */
-      topKFrequent(nums, k) {
-        const frequencyMap = nums.reduce((acc, value) => {
-            if (acc[value]) acc[value] +=1
+    topKFrequent(nums, k) {
+        const numsFreq = nums.reduce((acc, value) => {
+            if (acc[value]) acc[value]+=1
             else acc[value] = 1
-            return acc;
+            return acc
         }, {})
-        return Object.keys(frequencyMap).sort((a, b) => frequencyMap[b]-frequencyMap[a]).slice(0,k)
-    }
+        // make array of tuples, sort by the second item in the tuple (frequency)
+        // const numsFreqOrganized = Object.entries(numsFreq).sort((i, j) => j[1] - i[1])
+        // return numsFreqOrganized.splice(0, k).map(([value, _freq]) => Number(value))
 
+        // make buckets
+        const buckets = Array(nums.length + 1).fill().map(() => []);
+        // make results 
+        const results = []
+
+        // populate buckets, indexing by freq
+        for (let [value, freq] of Object.entries(numsFreq)) {
+            if (buckets[freq]) buckets[freq].push(value)
+            else buckets[freq] = [value]
+        }
+
+        // make results array, loop backwards from buckets 
+        for (let i = buckets.length-1; i >= 0; i --){
+            if (buckets[i]) results.push(...buckets[i])
+            if (results.length >= k) return results.slice(0, k)
+        }
+
+        return results
+
+    }
 }
